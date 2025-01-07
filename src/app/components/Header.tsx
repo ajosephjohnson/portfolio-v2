8675a2cf933bf7a72f22c7f9be71bd728b35e4cd
Alan Johnson <ajosephjohnson@gmail.com>
2025-01-07 00:42:05 -0600

    Update GitHub link in Header component to point to portfolio repository

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     return (
         <header className="absolute top-0 w-full flex justify-end">
             <a 
-                href="https://github.com/ajosephjohnson" 
+                href="https://github.com/ajosephjohnson/portfolio-v2" 
                 target="_blank" 
                 rel="noopener noreferrer"
                 className="h-16 w-16 sm:h-20 sm:w-20 p-3 sm:p-5 hover:opacity-50 transition-opacity duration-300 ease-in-out"
