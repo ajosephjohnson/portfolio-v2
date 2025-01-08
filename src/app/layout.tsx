b5b75a265a84f438c93ca93533742150c9d87a1e
Alan Johnson <ajosephjohnson@gmail.com>
2025-01-07 19:55:15 -0600

    Add @next/third-parties package and integrate Google Analytics in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { GoogleAnalytics } from '@next/third-parties/google'
+
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "./components/Header";
@@ -31,6 +33,7 @@ export default function RootLayout({
                 <Header />
                 {children}
             </body>
+            <GoogleAnalytics gaId="G-J8WPCMWEZK" />
         </html>
     );
 }
